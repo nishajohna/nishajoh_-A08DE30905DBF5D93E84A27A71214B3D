02d85b44827ae5022fb04dcc0644e5edbab2531e
nishajohna <148056357+nishajohna@users.noreply.github.com>
2023-10-16 15:15:01 +0530

    Add files via upload

---
+++ challenge3.1.py.py
@@ -0,0 +1,23 @@
+def
+
+linearsearchproduct(productlist, target
+
+product):
+
+indices=[]
+
+
+enumerate(productlist):
+
+for index, product in 
+enumerate(productlist):
+indices.append(index)
+
+return indices
+
+
+products =""
+target="shoes"
+result-linear searchproduct(products, targ
+et) print(result)
+
